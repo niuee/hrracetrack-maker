15dcdb6dac7571eb7a12029c267acc075f6642e1
niuee <vntchang@gmail.com>
2023-06-21 12:03:16 +0800

    Changed server port from reading from .env file to passing argument

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -6,6 +6,8 @@ import fs from "fs/promises";
 import { Bezier } from "bezier-js";
 
 require('dotenv').config();
+let serverPort = process.argv[2];
+// console.log("PORT:", serverPort);
 
 export type TrackData = {
     curves: CurveData[];
@@ -50,7 +52,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(path.resolve(__dirname, ".."), '/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = serverPort || process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
